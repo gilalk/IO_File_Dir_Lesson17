8dd51818ef69c2d639facd564e746c7654a85f51
gilalk <86377486+gilalk@users.noreply.github.com>
2021-11-08 18:20:48 +0200

    Add files via upload

---
+++ IOFilesAndDirectories_Lesson17/IOFilesAndDirectories_Lesson17/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace IOFilesAndDirectories_Lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Question 1
+            
+            var arr = Directory.GetLogicalDrives();
+            var arr1 = Directory.GetDirectories(arr[0]);
+
+            foreach (var item in arr1.Take(10))
+            {
+                Console.WriteLine(item);
+            }
+
+
+            // Question 2
+
+            GetBiggestFiles(@"C:\Users\gilal");
+
+            // Question 3
+
+            List<Student> students = new List<Student>()
+            {
+                new Student(32, "gil", "alkobi", 98),
+                new Student(14, "avia", "simhi", 100),
+                new Student(76, "ziv", "zimra", 90),
+                new Student(213, "eli", "ohana", 87),
+                new Student(89, "david", "levi", 92),
+                new Student(5, "ana", "miler", 71),
+            };
+
+            //var jsonStudents = JsonSerializer.Serialize(students);
+            //File.WriteAllText(@"students.json", jsonStudents);
+
+            // Question 4
+            string myFixLength = "";
+            foreach (var student in students)
+            {
+                myFixLength += student.WriteStudentsFixedLength() + "  ";
+            }
+
+            // Question 5
+
+            var text = "";
+            foreach (var student in students)
+            {
+                text += student.ToCSV() + Environment.NewLine;
+            }
+            File.WriteAllText(@"C:\Users\gilal\students.csv", text);
+
+            // Question 6 
+
+            var listFromCSV = File.ReadAllLines(@"C:\Users\gilal\students.csv").ToList();
+
+
+            // Question 7
+            // fixed length
+            // ניתן לבחור באורך כל רשומה מראש כך שלא חייב לבזבז הרבה מידע לשווא
+
+            // Question 8
+
+            // אין גמישות ברגע שהגדרנו את טווח התוים ולכן יש מגבלות
+
+            // Question 9
+
+            // קובץ בינארי מדויק יותר וקל יותר, לא מוגבל לתיאור על ידי טקסט אלא גם בתצורה שהקורא לא בהכרח מבין מה הוא קורא.
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// function for question 2
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        public static void GetBiggestFiles(string path)
+        {
+            DirectoryInfo myDir = new DirectoryInfo(path);
+            var myFiles = myDir.GetFiles();
+            var fileList = myFiles.OrderByDescending(file => file.Length);
+
+            foreach (var item in fileList.Take(3))
+            {
+                Console.WriteLine($"File Name: {item.Name}\nLast Change: {item.LastWriteTime}");
+            }
+        }
+    }
+}
---
+++ IOFilesAndDirectories_Lesson17/IOFilesAndDirectories_Lesson17/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOFilesAndDirectories_Lesson17
+{
+    class Student
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Grade { get; set; }
+
+        public Student(int iD, string firstName, string lastName, int grade)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            Grade = grade;
+        }
+
+
+        public string ToCSV()
+        {
+            string myStr = "";
+            myStr = string.Format("{0}, {1}, {2}, {3}", ID, FirstName, LastName, Grade);
+            return myStr;
+        }
+
+        public string WriteStudentsFixedLength()
+        {
+            string str;
+            str = string.Format("{0:9},{1:12},{2:20},{3:3}", ID.ToString(), FirstName, LastName, Grade.ToString());
+            return str;
+        }
+    }
+}
